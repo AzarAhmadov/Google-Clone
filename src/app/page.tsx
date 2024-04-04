d44ad92067d07506262979616416fef812519e6b
AzarAhmadov <ehmedovh111@gmail.com>
2024-04-04 10:44:38 +0400

    Added home header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import HomeHeader from '@/components/HomeHeader/HomeHeader'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      Home Page
-    </div>
+    <>
+      <HomeHeader />
+    </>
   )
 }
 
---
+++ src/components/HomeHeader/HomeHeader.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+import React from 'react'
+import { TbGridDots } from "react-icons/tb";
+
+const HomeHeader = () => {
+    return (
+        <header className='flex justify-end p-5 text-sm'>
+            <nav>
+                <ul className='flex space-x-4 items-center'>
+                    <li>
+                        <Link className='hover:underline' href={'https://mail.google.com'}> Gmail </Link>
+                    </li>
+                    <li>
+                        <Link className='hover:underline' href={'https://image.google.com'}> Images </Link>
+                    </li>
+                    <li>
+                        <TbGridDots className='bg-transparent hover:bg-gray-200 hover:cursor-pointer rounded-full text-4xl p-2' />
+                    </li>
+                    <li>
+                        <button className='bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-shadow'> Sign in </button>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default HomeHeader
