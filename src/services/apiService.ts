e83b40c9961bc5e3653997030249ad883c10a036
AzarAhmadov <ehmedovh111@gmail.com>
2024-04-04 14:42:21 +0400

    Created fetch user country

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,6 +1,24 @@
 export const fetchRandomWords = async () => {
-    const response = await fetch('https://random-word-api.herokuapp.com/word')
-    const data = await response.json()
-    if (!data) return
-    return data
-}
+    try {
+        const response = await fetch('https://random-word-api.herokuapp.com/word');
+        if (!response.ok) {
+            throw new Error('Failed to fetch random words');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching random words:', error);
+        return null;
+    }
+};
+
+export const fetchUserCountry = async () => {
+    try {
+        const response = await fetch('https://ipapi.co/json/');
+        const data = await response.json();
+        return data.country_name;
+    } catch (error) {
+        console.error('Error fetching user country:', error);
+        throw error;
+    }
+};
