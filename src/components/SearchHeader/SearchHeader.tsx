dbe6aaddd2ea9fa71f4c6a629c179b171ae51347
AzarAhmadov <ehmedovh111@gmail.com>
2024-04-04 15:13:21 +0400

    Created search header

---
+++ src/components/SearchHeader/SearchHeader.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react'
+
+const SearchHeader: FC = () => {
+    return (
+        <div>
+            SearchHeader
+        </div>
+    )
+}
+
+export default SearchHeader
