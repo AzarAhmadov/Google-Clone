0ad75ccef8fa952696cf1f4c593dbce7bd5b3cbb
AzarAhmadov <ehmedovh111@gmail.com>
2024-04-04 12:17:58 +0400

    Updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,11 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    screens: {
+      'sm': '300px',
+      'md': '960px',
+      'lg': '1440px',
+    },
   },
   plugins: [],
 };
