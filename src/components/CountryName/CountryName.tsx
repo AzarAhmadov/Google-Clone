3de64f6eb34f37fc6167a99763af2ed5bb520dcd
AzarAhmadov <ehmedovh111@gmail.com>
2024-04-04 14:42:39 +0400

    Fetch of country name done

--- src/components/CountryName/CountryName.tsx
+++ src/components/CountryName/CountryName.tsx
@@ -1,12 +1,30 @@
-import React from 'react'
+'use client'
 
-const CountryName = () => {
+import { fetchUserCountry } from '@/services/apiService';
+import React, { FC, useState, useEffect } from 'react';
+
+const CountryName: FC = () => {
+
+    const [userCountry, setUserCountry] = useState<string>('');
+
+    useEffect(() => {
+        const getUserCountry = async () => {
+            try {
+                const country = await fetchUserCountry();
+                setUserCountry(country);
+            } catch (error) {
+                console.log(error)
+            }
+        };
+
+        getUserCountry();
+    }, []);
 
     return (
         <span>
-            Azerbaijan
+            {userCountry || 'Loading...'}
         </span>
-    )
-}
+    );
+};
 
-export default CountryName
+export default CountryName;
