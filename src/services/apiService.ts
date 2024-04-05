309650144f0ba40e45b0ca22720183f02615d8ad
AzarAhmadov <ehmedovh111@gmail.com>
2024-04-05 10:48:15 +0400

    added google search api

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,3 +1,4 @@
+// Fetching random words
 export const fetchRandomWords = async () => {
     try {
         const response = await fetch('https://random-word-api.herokuapp.com/word');
@@ -12,11 +13,24 @@ export const fetchRandomWords = async () => {
     }
 };
 
+// Fetching which country the user is in
 export const fetchUserCountry = async () => {
     try {
         const response = await fetch('https://ipapi.co/json/');
         const data = await response.json();
         return data.country_name;
+    } catch (error) {
+        console.error('Error fetching user country:', error);
+        throw error;
+    }
+};
+
+// Fetching google search result
+export const fetchSearch = async (lectures: string) => {
+    try {
+        const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${lectures}`);
+        const data = await response.json();
+        return data.items;
     } catch (error) {
         console.error('Error fetching user country:', error);
         throw error;
