e6107c94f627acca84aaeee1da80e48eafba4ffe
AzarAhmadov <ehmedovh111@gmail.com>
2024-04-05 12:27:53 +0400

    Updated

--- src/components/SearchHeader/SearchHeader.tsx
+++ src/components/SearchHeader/SearchHeader.tsx
@@ -29,7 +29,7 @@ const SearchHeader: FC = () => {
                     <RiSettings3Line className='bg-transparent hover:bg-gray-200 p-2 text-4xl rounded-full cursor-pointer' />
                     <TbGridDots className='bg-transparent hover:bg-gray-200 p-2 text-4xl rounded-full cursor-pointer' />
                 </div>
-                <button className='md:relative md:top-0 md:right-0 absolute top-[18px] right-[10px] bg-blue-500 text-white md:px-6 md:py-2 py-1 px-4 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-shadow ml-2'>
+                <button className='md:relative md:top-0 md:right-0 absolute top-[20px] right-[10px] bg-blue-500 text-white md:px-6 md:py-2 py-1 px-4 font-medium text-[13px] lg:text-[17px] rounded-md hover:brightness-105 hover:shadow-md transition-shadow ml-2'>
                     Sign in
                 </button>
             </div>
--- src/components/SearchResults/SearchResults.tsx
+++ src/components/SearchResults/SearchResults.tsx
@@ -17,7 +17,7 @@ interface SearchResultsProps {
 const SearchResults: FC<SearchResultsProps> = ({ getSearchData }) => {
     return (
         <>
-            <div className='w-full mx-auto px-3 sm:pb-24 pb-40 sm:pl-[5%] md:pl-[14%] lg:pl-52'>
+            <div className='w-full mx-auto px-3 sm:pb-24 pb-40 sm:pl-[12px] md:pl-[14%] lg:pl-52'>
                 {/* <p className='text-gray-600 text-sm mb-5 mt-3'>
                     About {getSearchData?.searchInformation.formattedTotalResults} results (
                     {getSearchData?.searchInformation.formattedSearchTime} seconds)
