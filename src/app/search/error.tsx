367e3e15606f3928e31c23a3541afa44d4ab9066
AzarAhmadov <ehmedovh111@gmail.com>
2024-04-05 11:18:38 +0400

    Updated the Error component

--- src/app/search/error.tsx
+++ src/app/search/error.tsx
@@ -1,13 +1,29 @@
-'use client';
+'use client'
 
-const Error = () => {
+import { useEffect } from 'react'
+
+export default function Error({
+    error,
+    reset,
+}: {
+    error: Error & { digest?: string }
+    reset: () => void
+}) {
+    useEffect(() => {
+        console.error(error)
+    }, [error])
 
     return (
-        <div className='flex flex-col justify-center text-center items-center pt-10'>
-            <h1 className='text-3xl mb-4'>Something went wrong!</h1>
-            <button className='text-blue-500'>Try again</button>
+        <div className='text-center mt-11 flex-col flex items-center justify-center'>
+            <h2 className='text-[20px]'>Something went wrong!</h2>
+            <button
+                onClick={
+                    () => reset()
+                }
+                type="button"
+                className="text-white mt-4 bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+                Try again
+            </button>
         </div>
-    );
+    )
 }
-
-export default Error
