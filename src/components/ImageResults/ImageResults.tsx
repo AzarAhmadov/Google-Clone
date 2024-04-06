b8c680cddd32f38d5e0ca847ed92f900a2ae1a82
AzarAhmadov <ehmedovh111@gmail.com>
2024-04-06 09:47:16 +0400

    Updated

--- src/components/ImageResults/ImageResults.tsx
+++ src/components/ImageResults/ImageResults.tsx
@@ -16,7 +16,7 @@ interface Props {
 const ImageResults: React.FC<Props> = ({ getSearchImageData }) => {
     return (
         <div className='sm:pb-24 pb-40 mt-4'>
-            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-3 space-x-4'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-2 md:px-3 space-x-2 md:space-x-4'>
                 {getSearchImageData.map((result: ImageResult) => (
                     <div className='mb-8' key={result.link}>
                         <div className='group'>
@@ -24,16 +24,16 @@ const ImageResults: React.FC<Props> = ({ getSearchImageData }) => {
                                 <img
                                     src={result.link}
                                     alt={result.title}
-                                    className='h-60 group-hover:shadow-xl w-full object-contain transition-shadow duration-300'
+                                    className='h-30 lg:h-60 group-hover:shadow-xl w-full object-cover  transition-shadow duration-300'
                                 />
                             </Link>
                             <Link href={result.image.contextLink}>
-                                <h2 className='group-hover:underline truncate text-xl'>
+                                <h2 className='group-hover:underline truncate mt-2 md:text-xl text-[13px]'>
                                     {result.title}
                                 </h2>
                             </Link>
                             <Link href={result.image.contextLink}>
-                                <p className='group-hover:underline truncate text-gray-600'>
+                                <p className='group-hover:underline truncate text-[13px] md:text-[18px] text-gray-600'>
                                     {result.displayLink}
                                 </p>
                             </Link>
